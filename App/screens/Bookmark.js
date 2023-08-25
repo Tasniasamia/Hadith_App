83c6c5344a2a14e4b147b907682d4b976ac630c8
tasniaarhamarham <tasniaarhamarham@gmail.com>
2023-08-25 20:13:55 +0600

    bookmark_done

--- App/screens/Bookmark.js
+++ App/screens/Bookmark.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, Image, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, Image, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -26,6 +26,10 @@ const BookMark =() => {
       }
       fetchData();
     }, []);
+
+    const removeFromBookmark=()=>{
+        
+    }
     // const bookmarkCollection = async () => {
     //     try {
     //         const StorageData = await AsyncStorage.getItem('StoreData');
@@ -48,8 +52,7 @@ const BookMark =() => {
           justifyContent: 'center',
           alignItems: 'center',
           backgroundColor:"#e2e1e1" ,
-          
-       marginBottom:5
+          height:50,
         },
         image: {
           width: screenWidth - 10,
@@ -68,20 +71,37 @@ const BookMark =() => {
     return (
         <>
         <SafeAreaView style={{ marginHorizontal: 5, marginTop: 60 }}>
+
+
           {/* Image and Text here */}
-          <ScrollView contentContainerStyle={{ marginTop: 50, backgroundColor: "#dfeccd" }}>
+
+
+          <View style={styles.container}>
+      <Image
+        source={require("../assets/hadith_name.png")}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text style={styles.text}>Hadith</Text>
+    </View>
+          <ScrollView contentContainerStyle={{ marginTop: 80, backgroundColor: "#dfeccd" }}>
             {/* Display BookItem */}
             {BookItem.map((index, index2) => (
-              <View key={index2} style={styles.bookmarkItem}>
-                <View style={styles.header}>
-                  <Text style={styles.headerText}>{index.name}</Text>
-                  <View style={styles.icons}>
-                    <Ionicons name="bookmark-sharp" size={20} color="gray" style={styles.icon} />
-                    <Ionicons name="share-social-sharp" size={20} color="gray" style={styles.icon} />
-                  </View>
+              <View key={index2} style={{backgroundColor:"#ecf4e3",marginVertical:10,marginHorizontal:10,paddingVertical:10,paddingHorizontal:10}}>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Text style={{ fontSize: 20, color: "green", fontWeight: "bold" }}>
+{index.name}</Text>
+<View style={{ flexDirection: "row" }}>
+    <TouchableOpacity >
+      <Ionicons name="bookmark-sharp" size={20} color="gray" style={{ paddingVertical: 5, color: "green", fontWeight: "bold" }} />
+    </TouchableOpacity>
+    <TouchableOpacity style={{marginLeft:10}} >
+        <Ionicons name="share-social-sharp" size={20} color="gray" style={{ paddingVertical: 5, color: "green", fontWeight: "bold" }} />
+    </TouchableOpacity>
+  </View>
                 </View>
-                <Text style={styles.narrate}>{index.narrate}</Text>
-                <Text style={styles.text}>{index.text}</Text>
+                <Text >{index.narrate}</Text>
+                <Text >{index.text}</Text>
               </View>
             ))}
           </ScrollView>
